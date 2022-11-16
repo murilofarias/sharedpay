f49deeafa5ade777f5106fcee3391572fab52ea5
murilofarias <murilo145farias@gmail.com>
2022-11-16 11:49:15 -0300

    first commit

---
+++ src/main/java/br/com/murilofarias/sharedpay/SharedpayApplication.java
@@ -0,0 +1,13 @@
+package br.com.murilofarias.sharedpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharedpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharedpayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/murilofarias/sharedpay/SharedpayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.murilofarias.sharedpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharedpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
