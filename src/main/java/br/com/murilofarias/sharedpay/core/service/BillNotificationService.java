47a527230e743f112cdc2cf5928dd8a771b3246d
murilofarias <murilo145farias@gmail.com>
2022-11-21 11:35:40 -0300

    Add credit to user owner of the bill when payment is fulfilled

--- src/main/java/br/com/murilofarias/sharedpay/core/service/BillNotificationService.java
+++ src/main/java/br/com/murilofarias/sharedpay/core/service/BillNotificationService.java
@@ -2,13 +2,16 @@ package br.com.murilofarias.sharedpay.core.service;
 
 import br.com.murilofarias.sharedpay.core.model.Payment;
 import br.com.murilofarias.sharedpay.core.model.PaymentStatus;
+import br.com.murilofarias.sharedpay.core.model.User;
 import br.com.murilofarias.sharedpay.core.plug.PaymentRepository;
 import br.com.murilofarias.sharedpay.core.plug.PaymentService;
+import br.com.murilofarias.sharedpay.core.plug.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -28,15 +31,24 @@ public class BillNotificationService {
     @Autowired
     private PaymentRepository paymentRepository;
 
+    @Autowired
+    private UserRepository userRepository;
+
     public String checkPaymentStatus(String paymentId){
 
         taskScheduler.submit(new Runnable() {
             @Override
+            @Transactional
             public void run() {                try {
                     PaymentStatus status = paymentService.requestPaymentStatus(paymentId);
                     Payment payment = paymentRepository.findById(parseLong(paymentId)).get();
                     payment.setStatus(status);
                     paymentRepository.save(payment);
+                    if(status.equals(PaymentStatus.FULFILLED))
+                    {
+                        User user = userRepository.findByPersonCpf(payment.getDebtor().getCpf()).get();
+                        user.addCredit(payment.getValue());
+                    }
                     logger.info("Notification of Payment status of id " + paymentId + " is " + status.toString());
 
                 }catch(Exception e){
